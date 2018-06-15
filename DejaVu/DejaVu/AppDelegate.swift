dc11b420e8a27cdf48f730faf3a48816a18bb05b
Tomas Silveira Salles <ts@imagineon.de>
2018-06-16 01:24:19 +0200

    Registered restricted API key with Google Maps and added it to the project.

--- DejaVu/DejaVu/AppDelegate.swift
+++ DejaVu/DejaVu/AppDelegate.swift
@@ -7,12 +7,21 @@
 //
 
 import UIKit
+import GoogleMaps
+import GooglePlaces
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
+	static let googleMapsAPIKey = "AIzaSyA5jLbFWlsjUnxm5IwZ0oKycwtqUHHu_bk"
+	
 	var window: UIWindow?
 
-	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool
+	{
+		// Provide API key for Google Maps:
+		GMSServices.provideAPIKey(AppDelegate.googleMapsAPIKey) // Maps SDK
+		GMSPlacesClient.provideAPIKey(AppDelegate.googleMapsAPIKey) // Places SDK
+		
 		return true
 	}
 }
