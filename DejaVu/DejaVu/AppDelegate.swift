4d9d07ce64b5b142a003720beb8b8d516bb98770
Tomas Silveira Salles <ts@imagineon.de>
2018-06-16 00:30:06 +0200

    Created initial xcode project.

---
+++ DejaVu/DejaVu/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  DejaVu
+//
+//  Created by Tomás Silveira Salles on 16.06.18.
+//  Copyright © 2018 TwoA. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+	var window: UIWindow?
+
+	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+		return true
+	}
+}
