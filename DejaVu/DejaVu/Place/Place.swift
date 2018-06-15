c2585e183172905fd1a210c88e2f08429c23831f
Tomas Silveira Salles <ts@imagineon.de>
2018-06-16 00:55:52 +0200

    Added group and struct for the main model: `Place`.

---
+++ DejaVu/DejaVu/Place/Place.swift
@@ -0,0 +1,12 @@
+//
+//  Place.swift
+//  DejaVu
+//
+//  Created by Tomás Silveira Salles on 16.06.18.
+//  Copyright © 2018 TwoA. All rights reserved.
+//
+
+import Foundation
+
+struct Place {
+}
